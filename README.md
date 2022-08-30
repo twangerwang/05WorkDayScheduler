079fbd0df09be2ae19c67b8e6aae6afac1457319
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-08-30 13:16:25 -0500

    fixed local storage retrieval, finished readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # 05WorkDayScheduler
+Create a workday scheduler that will take text and store in local storage with the hour as key. The data will persist on the page even after refresh. There is also a running clock with the current time to colorblock the scheduler with grey for time past, red for current hour, and green for hours coming up. 
+
+## Description
+*on page load a blank schedule is presented with 9am-5pm.
+
+*User can fill out the schedule and save using the button to the right of each text field.
+
+*The data is stored using local storage and the time block as key for retrieval.
+
+*Upon refreshing of the page, data is pulled from local storage using the time as key to put them in the correct time slots. 
+
+## Mock-up
+The images below shows the webpage created
+![alt text](images/workschedule.png)
+
+## Location
+https://twangerwang.github.io/05WorkDayScheduler/
+
+## Credits
+Original code provided by University of Texas Coding Bootcamp
+
+## License
+MIT License
+
+Copyright (c) 2022 Teddy Wang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
 var displayTimeEl = $("#currentDay");
 var saveButton = $(".btn");
-
-
-
 //displays current month day year hour minute seconds
 function displayTime() {
     var now = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
     displayTimeEl.text(now);
 }
-
 //determines if the hour is past, present, or future
 function hourStatus() {
     var currentHour = moment().hour(); //extracts the current hour from the time
-    // console.log("currentHour", currentHour);
     $(".time-block").each(function(){ //selects the id of each hour block and compares against current hour
         var blockHour = parseInt($(this).attr("id"));
         var hourCol = $(this).attr("id");
@@ -21,9 +16,6 @@ function hourStatus() {
         if (ampm==="pm" && blockHour!=12){  //makes the hour blocks into military time
             blockHour+=12;
         }
-        // console.log("blockHour", blockHour);
-        // console.log("hourCol", hourCol);
-        // console.log(this);
         if (currentHour>blockHour){
             $(this).addClass("past");
         } else {
@@ -35,22 +27,23 @@ function hourStatus() {
         }
     })
 }
-
+//saves text entered into local storage on click with time as key
 $(".btn").on("click", function() {
     var textentered = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-    // console.log(test);
-    // console.log(textentered);
-
-    localStorage.setItem(time, JSON.stringify(textentered));
+    localStorage.setItem(time, textentered);    
 })
-
-    
-// })
 
 hourStatus();
 setInterval(displayTime, 1000);
-
-
-
+//retrieve data stored in local storage back on schedule upon refresh of webpage 
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
 })
